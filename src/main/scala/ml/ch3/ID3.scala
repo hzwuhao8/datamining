e4134e3123ce42349e741293fe40492e5fd9dc58
wuhao <wuhao@bokee.net>
2015-12-07 22:09:08 +0800

    运用 训练后的  决策树

--- src/main/scala/ml/ch3/ID3.scala
+++ src/main/scala/ml/ch3/ID3.scala
@@ -14,7 +14,7 @@ object ID3 extends App {
 
   def splitDataSet(dataSet: IndexedSeq[IndexedSeq[Any]], axis: Int, value: Any): IndexedSeq[IndexedSeq[Any]] = {
     dataSet.filter(row => row(axis) == value).map { row =>
-     
+
       val (p1, p2) = row.splitAt(axis)
       p1 ++ p2.drop(1)
     }
@@ -23,70 +23,100 @@ object ID3 extends App {
   def chooseBestFeaturetoSplit(dataSet: IndexedSeq[IndexedSeq[Any]]): (Int, Double) = {
     val numberFeatures = dataSet.head.size - 1
     val baseEntropy = calcShannoneEnt(dataSet)
-  
-    val d2  =  for( i <-  0 until numberFeatures) yield{
-      val uniqueValus = dataSet.map( row => row(i)).distinct
-      
-      val d = for( v <- uniqueValus) yield{
-        val subDataSet = splitDataSet(dataSet , i, v)
-        val prob = subDataSet.size.toDouble  / dataSet.size
+
+    val d2 = for (i <- 0 until numberFeatures) yield {
+      val uniqueValus = dataSet.map(row => row(i)).distinct
+
+      val d = for (v <- uniqueValus) yield {
+        val subDataSet = splitDataSet(dataSet, i, v)
+        val prob = subDataSet.size.toDouble / dataSet.size
         prob * calcShannoneEnt(subDataSet)
       }
       val newEntropy = d.sum
       val infoGain = baseEntropy - newEntropy
-      (i,infoGain)
+      (i, infoGain)
     }
     println(d2)
     d2.sortBy(_._2).reverse.head
-    
+
   }
 
-  def major[A]( col: IndexedSeq[A]): A = {
-    val t1 = col.groupBy { x => x }.map( kv => kv._1 -> kv._2.size).toList
-    val h =  t1.sortBy( kv => kv._2).reverse.head
+  def major[A](col: IndexedSeq[A]): A = {
+    val t1 = col.groupBy { x => x }.map(kv => kv._1 -> kv._2.size).toList
+    val h = t1.sortBy(kv => kv._2).reverse.head
     h._1
   }
-  
-  def cols(dataSet: IndexedSeq[IndexedSeq[Any]] , index: Int) :IndexedSeq[Any] = {
-    dataSet.map( row => row(index))
+
+  def cols(dataSet: IndexedSeq[IndexedSeq[Any]], index: Int): IndexedSeq[Any] = {
+    dataSet.map(row => row(index))
   }
-  def colsSize(dataSet: IndexedSeq[IndexedSeq[Any]] ) : Int =   dataSet.head.size
-  
-  def classListVal(dataSet: IndexedSeq[IndexedSeq[Any]]) = cols(dataSet , colsSize(dataSet)-1)
-  
+  def colsSize(dataSet: IndexedSeq[IndexedSeq[Any]]): Int = dataSet.head.size
+
+  def classListVal(dataSet: IndexedSeq[IndexedSeq[Any]]) = cols(dataSet, colsSize(dataSet) - 1)
+
   def createTree(dataSet: IndexedSeq[IndexedSeq[Any]], labels: IndexedSeq[Symbol]): Any = {
     println("\n\n" + "-" * 60)
     println(s"dataSet=${dataSet}")
     println(s"labels=${labels}")
     val classList = classListVal(dataSet)
     println(s"classList=${classList}")
-    
-    val res = if( classList.forall { x => x== classList.head }){
-       classList.head
-    }else if( dataSet.head.size == 1 ){
-       major( classList)
-    }else{
+
+    val res = if (classList.forall { x => x == classList.head }) {
+      classList.head
+    } else if (dataSet.head.size == 1) {
+      major(classList)
+    } else {
       val bestFeat = chooseBestFeaturetoSplit(dataSet)
       val bestFeatLabel = labels(bestFeat._1)
       println(s"bestFeatLabel=${bestFeatLabel}")
-      val subLabel =  labels.filter( _ != bestFeatLabel)
-      val featValues = dataSet.map(row => row( bestFeat._1))
+      val subLabel = labels.filter(_ != bestFeatLabel)
+      val featValues = dataSet.map(row => row(bestFeat._1))
       val uniqueVals = featValues.distinct
       println(s"uniqueVals=${uniqueVals}")
-      val x = uniqueVals.map{ value => 
-          val k = value
-          val v = createTree(splitDataSet(dataSet, bestFeat._1, value),subLabel)
-          println(s"kv=${ k -> v }")
-          k->v
+      val x = uniqueVals.map { value =>
+        val k = value
+        val v = createTree(splitDataSet(dataSet, bestFeat._1, value), subLabel)
+        println(s"kv=${k -> v}")
+        k -> v
       }
-      val myTree = Map(bestFeatLabel ->  x )
+      val myTree = Map(bestFeatLabel -> x.toMap)
       myTree
     }
     println(res)
-    res 
-    
+    res
+
   }
-  
+
+  def classify(inputTree2: Any, labels: IndexedSeq[Symbol], testVec: IndexedSeq[Any]): Symbol = {
+    inputTree2 match {
+      case inputTree: Map[_, _] =>
+        println( inputTree)
+        val firstStr = inputTree.keys.head
+        val secondDict = inputTree(firstStr)
+        val featIndex = labels.indexOf(firstStr)
+        println(s"firstStr=${firstStr}")
+        println(s"secondDict=${secondDict}")
+        println(s"featIndex=${featIndex}")
+        secondDict match {
+          case dict: Map[_, _] => val sq = for (key <- dict.keys) yield {
+            val classLabel = if (testVec(featIndex) == key) {
+              dict(key) match {
+                case dd: Map[_, _] => classify(dd, labels, testVec)
+                case x : Symbol => x
+              }
+            }else{
+              'UNKNOWN
+            }
+            classLabel
+          }
+          sq.filterNot( _ == 'UNKNOWN).headOption.getOrElse('UNKNOWN)
+          
+          case x: Symbol => x
+        }
+    }
+
+  }
+
   val dataSet = IndexedSeq(
 
     IndexedSeq(1, 1, 'yes),
@@ -97,18 +127,27 @@ object ID3 extends App {
   val s1 = calcShannoneEnt(dataSet)
   println(s"s1=${s1}")
 
-  val s2 = calcShannoneEnt( dataSet.drop(1).+:( IndexedSeq(1, 1, 'maybe)) )
+  val s2 = calcShannoneEnt(dataSet.drop(1).+:(IndexedSeq(1, 1, 'maybe)))
   println(s"s2=${s2}")
   println(splitDataSet(dataSet, 0, 1))
   println(splitDataSet(dataSet, 0, 0))
-  
+
   println(chooseBestFeaturetoSplit(dataSet))
-  
-  println( major( dataSet.map( row => row.last)))
-  
+
+  println(major(dataSet.map(row => row.last)))
+
   val labels = IndexedSeq('no_surfacing, 'flippers)
-  
+
   val r = createTree(dataSet, labels)
   println(r)
-  
+  val c1 = r match {
+    case inputTree: Map[_, _] => 
+      val dataSet = IndexedSeq(
+          IndexedSeq(0,0)
+      )
+      dataSet.map{ row => classify(inputTree, labels, row )}
+      
+  }
+  println(s"c1=${c1}")
+
 }
