7a3f475f89aa8bda81767a38ce3706471d31a5a0
wuhao <hzwuhao8@gmail.com>
2015-12-10 15:33:45 +0800

    SVM 算法 处理 例子数据，  不是全部是匹配的 是 这种数据形式不 适合用 svm? 还是 问题本身，不适合svm ?

--- src/main/scala/spark/Tree.scala
+++ src/main/scala/spark/Tree.scala
@@ -2,11 +2,15 @@ package spark
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-
 import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.DenseVector
+import org.apache.spark.mllib.classification.{ LogisticRegressionWithSGD, SVMModel }
+import org.apache.spark.mllib.classification.{ NaiveBayes, NaiveBayesModel }
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.tree.RandomForest
 object Tree extends App {
   val appName = "Tree"
   val conf = new SparkConf().setAppName(appName).setMaster("local")
@@ -17,20 +21,73 @@ object Tree extends App {
   val dd5 = d5.map(row => LabeledPoint(row._1, new DenseVector(row._2.toArray)))
   val trainingData = sc.makeRDD(dd5)
   val numClasses = 2
-  val categoricalFeaturesInfo = Map[Int, Int](0 -> 2, 1 ->3)
+  val categoricalFeaturesInfo = Map[Int, Int](0 -> 2, 1 -> 3)
   val impurity = "Gini".toLowerCase()
   val maxDepth = 5
   val maxBins = 32
 
-  val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-    impurity, maxDepth, maxBins)
+  {
+    val model = DecisionTree.trainClassifier(trainingData, numClasses,
+      categoricalFeaturesInfo, impurity, maxDepth, maxBins)
 
-  println("Learned classification tree model:\n" + model.toDebugString)
-  val test = d5.map{row => 
-    val p = model.predict( new DenseVector(row._2.toArray) )
-    (row._1 , p)
+    println("Learned classification tree model:\n" + model.toDebugString)
+
+    val test = d5.map { row =>
+      val p = model.predict(new DenseVector(row._2.toArray))
+      (row._1, p)
+    }
+    test.foreach(println)
   }
-  test.foreach(println)
+
+  {
+    val model = DecisionTree.trainRegressor(trainingData,
+      categoricalFeaturesInfo, "variance", maxDepth, maxBins)
+    println("Learned classification tree model:\n" + model.toDebugString)
+    val test = d5.map { row =>
+      val p = model.predict(new DenseVector(row._2.toArray))
+      (row._1, p)
+    }
+    test.foreach(println)
+  }
+
+  {
+    val numTrees = 3
+    val featureSubsetStrategy = "auto"
+    val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+
+    println("Learned classification tree model:\n" + model.toDebugString)
+    val test = d5.map { row =>
+      val p = model.predict(new DenseVector(row._2.toArray))
+      (row._1, p)
+    }
+    test.foreach(println)
+
+  }
+
+  {
+
+    val model = NaiveBayes.train(trainingData, lambda = 1.0, modelType = "multinomial")
+
+    println("Learned classification tree model:\n" + model.toString())
+    val test = d5.map { row =>
+      val p = model.predict(new DenseVector(row._2.toArray))
+      (row._1, p)
+    }
+    test.foreach(println)
+  }
+
+  {
+    val model = new LogisticRegressionWithLBFGS()
+      .setNumClasses(2)
+      .run(trainingData)
+  println("Learned classification tree model:\n" + model.toString())
+    val test = d5.map { row =>
+      val p = model.predict(new DenseVector(row._2.toArray))
+      (row._1, p)
+    }
+    test.foreach(println)
+  }
   sc.stop()
 
 }
