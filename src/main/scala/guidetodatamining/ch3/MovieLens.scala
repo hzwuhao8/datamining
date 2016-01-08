f96906e996c27f6eef9671b1185e261583a0c670
wuhao <hzwuhao8@gmail.com>
2016-01-08 15:39:45 +0800

    u.item 的数据 有 非法字符 scala 延迟执行 ， 无法通过  try catch  进行过滤 最后用  commons.io 中的 FileUtils 进行了 读取

---
+++ src/main/scala/guidetodatamining/ch3/MovieLens.scala
@@ -0,0 +1,70 @@
+package guidetodatamining.ch3
+
+import guidetodatamining.ch2.Recommender.UserMap
+
+/**
+ * 最小的数据集的 处理
+ * data/ml-100k
+ */
+object MovieLens extends util.Log {
+  val path = "data/ml-100k/"
+  val ratingfile = path + "u.data"
+  val userfile = path + "u.user"
+  val moviefile = path + "u.item"
+
+  def main(args: Array[String]) {
+    val data = loadR()
+    val item = loadM()
+    val r = new SlopeOne(data)
+
+    r.recommond("1").map { case (mid, v) => (mid, item.getOrElse(mid, mid), v) }.foreach(println)
+
+  }
+
+  def loadR(): UserMap = {
+    val lines = scala.io.Source.fromFile(ratingfile).getLines()
+    val seq = lines.map { line =>
+      val fields = line.split("\t")
+      val Array(uid, itemid, rating, _) = fields
+      (uid -> (itemid -> rating.toDouble))
+
+    }
+
+    val z0 = Map[String, Map[String, Double]]()
+    seq.foldLeft(z0) { (a, b) =>
+      a.get(b._1) match {
+        case None => a ++ Map(b._1 -> Map(b._2._1 -> b._2._2)) // 还没有任何评分
+        case Some(d) => d.get(b._2._1) match {
+          case None     => a ++ Map(b._1 -> d.+(b._2._1 -> b._2._2)) // 已经对其他书进行了评分
+          case Some(dd) => a ++ Map(b._1 -> d.+(b._2._1 -> (dd + b._2._2))) //重复评分，好像是不可能的
+        }
+      }
+    }
+
+  }
+
+  def loadM(): Map[String, String] = {
+    val lines = org.apache.commons.io.FileUtils.readLines(new java.io.File(moviefile))
+
+    val seq = scala.collection.convert.WrapAsScala.asScalaBuffer(lines).flatMap { line =>
+      try {
+        
+        val fields = java.net.URLDecoder.decode(line).split("\\|")
+        
+        val res = if (fields.size >= 2) {
+          val mid = fields(0)
+          val name = fields(1)
+          Some((mid -> name))
+        } else {
+          None
+        }
+
+        res
+      } catch {
+        case _: Exception => None
+      }
+    }
+    seq.toMap
+  }
+
+}
--- src/main/scala/guidetodatamining/ch3/SlopeOne.scala
+++ src/main/scala/guidetodatamining/ch3/SlopeOne.scala
@@ -2,59 +2,88 @@ package guidetodatamining.ch3
 
 import guidetodatamining.ch2.Recommender.UserMap
 
-object SlopeOne  extends util.Log{
-  
-  
-  val users2: UserMap = Map("Amy"-> Map("Taylor Swift"-> 4, "PSY"-> 3, "Whitney Houston"-> 4),
-          "Ben"-> Map("Taylor Swift"-> 5, "PSY"-> 2),
-          "Clara"-> Map("PSY"-> 3.5, "Whitney Houston"-> 4),
-          "Daisy"-> Map("Taylor Swift"-> 5, "Whitney Houston"-> 3))
+object SlopeOne extends util.Log {
+
+  val users2: UserMap = Map("Amy" -> Map("Taylor Swift" -> 4, "PSY" -> 3, "Whitney Houston" -> 4),
+    "Ben" -> Map("Taylor Swift" -> 5, "PSY" -> 2),
+    "Clara" -> Map("PSY" -> 3.5, "Whitney Houston" -> 4),
+    "Daisy" -> Map("Taylor Swift" -> 5, "Whitney Houston" -> 3))
 
   
-  def dev(i: String , j: String, data: UserMap) : (Double,Int) = {
-    if( i==j){
-      (0.0,0)
-    }else{
-      val seq = data.flatMap{ case(u , v) =>
-        if( v.contains(i) && v.contains(j)){
-          val ui = v(i)
-          val uj = v(j)
-          Some(ui -uj)
-        }else{
-          None
-        }
+    
+  def main(args: Array[String]) {
+    val r = new SlopeOne(users2)
+    val s = r.devs
+    s.foreach(println)
+    val ben = "Ben"
+    val res = r.pui(users2(ben ), "Whitney Houston" )
+    println(s"res=${res}")
+    val res2 = r.recommond(ben)
+    res2.foreach(println)
+  }
+}
+
+class  SlopeOne(data: UserMap) extends util.Log {
+  lazy val  devs : Seq[(String, String, (Double, Int))] = sDev()
+  def dev(i: String, j: String): (Double, Int) = {
+    if (i == j) {
+      (0.0, 0)
+    } else {
+      val seq = data.flatMap {
+        case (u, v) =>
+          if (v.contains(i) && v.contains(j)) {
+            val ui = v(i)
+            val uj = v(j)
+            Some(ui - uj)
+          } else {
+            None
+          }
       }
-      if(seq.size ==0){
-          (0.0,0)
-      }else{
-      ( (seq.sum) /seq.size , seq.size)
+      if (seq.size == 0) {
+        (0.0, 0)
+      } else {
+        ((seq.sum) / seq.size, seq.size)
       }
     }
-     
+
   }
-  
-  def pui(u: Map[String,Double] , j: String  , s: Seq[(String,String , (Double , Int))]   ): Double ={
-    val seq = u.flatMap{ case( k,v) => if( k==j ) None else Some( k ) }
-    val d2 = seq.flatMap{ i  => s.filter( p=> p._1 == j && p._2 == i).map(   _._3._2) }.sum
-    
-    val d1 = seq.map{ i => 
-      val h =  s.filter( p=> p._1 == j && p._2 == i).head
+
+  def sDev(): Seq[(String, String, (Double, Int))] = {
+    val movies = data.flatMap { case (u, v) => v.map(_._1) }.toSet.toSeq
+    val pairList = movies.flatMap(x => movies.map(y => (x, y)))
+    val s = pairList.map { case (m1, m2) => (m1, m2, dev(m1, m2)) }
+    s
+  }
+  def pui(u: Map[String, Double], j: String): Double = {
+    val seq = u.flatMap { case (k, v) => if (k == j) None else Some(k) }
+    val d2 = seq.flatMap { i => devs.filter(p => p._1 == j && p._2 == i).map(_._3._2) }.sum
+
+    val d1 = seq.map { i =>
+      val h = devs.filter(p => p._1 == j && p._2 == i).head
       val devji = h._3._1
       val ui = u(i)
       val cji = h._3._2
-      (devji + ui ) * cji
-      
+      (devji + ui) * cji
+
     }.sum
-    d1 /d2     
-   
+    d1 / d2
+
   }
-  def main(args: Array[String]) {
-    val movies = users2.flatMap{ case( u ,v) => v.map(_._1)}.toSet.toSeq
-    val pairList = movies.flatMap( x => movies.map(y=> (x,y)) )
-    val s =   pairList.map{case (m1,m2) => (m1,m2, dev(m1,m2,users2))}   
-    s.foreach(println)
-    
-    val res =  pui( users2("Ben") , "Whitney Houston" , s)
-    println(s"res=${res}")
+ 
+  /**
+   * 推荐
+   */
+  def recommond(u:String   ): Seq[(String , Double)] = {
+     
+      data.get(u) match{
+      case None => Seq()
+      case Some(udata) => 
+        //取得 全部的 movies
+        //计算 还有 评价的 movie
+        // 按评价从高到低排列
+        val movies = devs.map(_._1).toSet
+        val mayBe = ( movies -- udata.keys)
+        mayBe.map( m => (m , pui( udata , m  ))).toSeq.sortBy( _._2).reverse 
+    }
   }
 }
