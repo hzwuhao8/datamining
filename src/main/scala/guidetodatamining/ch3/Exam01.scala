ef93a926a1af2207a353f1a2621061a6964384e5
wuhao <hzwuhao8@gmail.com>
2016-01-08 18:09:45 +0800

    余弦相似度 矩阵 进行par  优化

--- src/main/scala/guidetodatamining/ch3/Exam01.scala
+++ src/main/scala/guidetodatamining/ch3/Exam01.scala
@@ -15,13 +15,61 @@ object Exam01 extends util.Log {
     "Tori" -> Map("Kacey Musgraves" -> 5, "Imagine Dragons" -> 4,
       "Daft Punk" -> 5, "Fall Out Boy" -> 3))
 
-  def computeSimilarity(band1: String, band2: String, userRatings: UserMap): Double = {
-    val averages = userRatings.map {
+  def main(args: Array[String]) {
+    val r =new CosRecommend(users3)
+    r.sdev.foreach(println)
+
+    val user = "David"
+    val movie = "Kacey Musgraves"
+    val res = r.pui(users3(user), movie)
+    log.info(s"${user} -> ${movie} = ${res}")
+    for ((movie, v) <- users3(user)) {
+      log.info(s"已知是 ${v}； ${user} -> ${movie} = ${r.pui(users3(user), movie)}")
+    }
+  }
+
+}
+
+/**
+ * 根据余弦相似度 推荐
+ */
+
+class CosRecommend(data: UserMap) extends util.Log {
+  lazy val sdev = devs()
+  
+  def zipself[A](x: List[A]):List[(A,A)] ={
+    x match{
+      case Nil => Nil
+      case x::Nil => Nil
+      case h::tail => tail.map( x=> (h,x)) ::: zipself(tail)
+    }
+    
+  }
+  
+  
+  def devs(): Map[(String, String), Double] = {
+    val bandSeq = data.flatMap { case (k, v) => v.keySet }.toSet.toList
+    log.debug(s"bandSeq=${bandSeq.take(10)}")
+    val pairList = zipself(bandSeq)
+    
+    
+    // 相似度矩阵
+    val seq = pairList.par.map { case (b1, b2) => (b1, b2) -> computeSimilarity(b1, b2) }
+    val xs = new Array[((String,String), Double)](pairList.size )
+    seq.copyToArray(xs)
+    val m1 = xs.toMap
+    val s1 = pairList.map{ case(b1,b2) =>  (b2,b1) -> m1(b1,b2)}
+    val s2 = bandSeq.map{  b=> (b,b)->1.0}
+    m1 ++ s1 ++ s2
+  }
+  def computeSimilarity(band1: String, band2: String): Double = {
+    val averages = data.par.map {
       case (k, v) =>
         k -> v.values.sum / v.size
     }
+    
     val z = (0.0, 0.0, 0.0)
-    val (num, dem1, dem2) = userRatings.flatMap {
+    val (num, dem1, dem2) = data.flatMap {
       case (user, ratings) =>
         if (ratings.contains(band1) && ratings.contains(band2)) {
           val avg = averages(user)
@@ -51,7 +99,7 @@ object Exam01 extends util.Log {
   /**
    * u 对 i 的 可能的评价
    */
-  def pui(u: Map[String, Double], i: String, s: Seq[(String, String, Double)]): Double = {
+  def pui(u: Map[String, Double], i: String): Double = {
     u.get(i) match {
       case Some(v) => v
       case None =>
@@ -60,8 +108,8 @@ object Exam01 extends util.Log {
         val (min, max) = (seq1.min, seq1.max)
         // 数据归一
         val run = u.map { case (k, v) => k -> nr(v, min, max) }
-        val sin = s.flatMap {
-          case Tuple3(k1, k2, d) =>
+        val sin = sdev.flatMap {
+          case Tuple2((k1, k2), d) =>
             if (k1 != i || k2 == i) {
               None
             } else {
@@ -74,25 +122,6 @@ object Exam01 extends util.Log {
 
         val d = d1 / d2
         rn(d, min, max)
-    }
-  }
-  def main(args: Array[String]) {
-    val bandSeq = users3.flatMap { case (k, v) => v.keySet }.toSet.toSeq
-    log.debug(s"bandSeq=${bandSeq}")
-    val pairList = bandSeq.flatMap { x =>
-      bandSeq.flatMap { y =>
-        if (x == y) Some((x, y)) else Some((x, y))
-      }
-    }.toList
-    // 相似度矩阵
-    val seq = pairList.map { case (b1, b2) => (b1, b2, computeSimilarity(b1, b2, users3)) }
-    seq.foreach(println)
-    val user = "David"
-    val movie = "Kacey Musgraves"
-    val res = pui(users3(user), movie, seq)
-    log.info(s"${user} -> ${movie} = ${res}")
-    for ((movie, v) <- users3(user)) {
-      log.info(s"已知是 ${v}； ${user} -> ${movie} = ${pui(users3(user), movie, seq)}")
     }
   }
 
