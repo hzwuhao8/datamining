0af725d81184ebc210ca6f1d1d78d810c993fcce
wuhao <hzwuhao8@gmail.com>
2016-01-13 16:05:18 +0800

    代码整理， 抽取方法 便于 扩展

--- src/main/scala/guidetodatamining/ch6/Ihealth.scala
+++ src/main/scala/guidetodatamining/ch6/Ihealth.scala
@@ -4,25 +4,30 @@ import scala.io.Source
 object Ihealth extends util.Log {
 
   val path = "data/iHealth/i-0"
-  def main(args: Array[String]) {
-    val i = 1
-    val lines = Source.fromFile(s"${path}${i}").getLines().toList
+
+  def read(filename: String): List[List[String]] = {
+    val lines = Source.fromFile(filename).getLines().toList
     val data = lines.map(_.split("\t").toList)
     data.foreach(println)
-    val columns = 5
+    data
+  }
+  def getData1(data: List[List[String]]) = {
+    val columns = data.head.size
     val data1 = for (i <- 0 to columns - 1) yield { data.groupBy(x => x(i)).map { case (k, v) => k -> v.size } }
     data1.foreach(println)
-
-    val data2 = data.groupBy { x => x.last }.map {
+    data1
+  }
+  def getData2(data: List[List[String]], labelindex: Int) = {
+    val columns = data.head.size
+    val data2 = data.groupBy { x => x(labelindex) }.map {
       case (k, v) =>
         val total = v.size
-        val x = for (i <- 0 to columns - 2) yield {
+        val x = for (i <- 0 to columns - 1; if i != labelindex) yield {
           val g = v.groupBy(x => x(i))
           g.map {
             case (k, v) =>
-              
               if (v.size == 0) {
-                k -> ( 1.0/(total + g.size ) )
+                k -> (1.0 / (total + g.size))
               } else {
                 k -> (v.size.toDouble / total)
               }
@@ -31,13 +36,23 @@ object Ihealth extends util.Log {
         k -> x
     }
     data2.foreach(println)
-    val kList = Seq("health", "moderate", "aggressive", "yes")
-
-    val data3 = data1(columns - 1)
-    classify(kList, data2, data3)
+    data2
   }
 
-  def classify(kList: Seq[String], data2: Map[String, Seq[Map[String, Double]]], data3: Map[String, Int]): (String, Double) = {
+  def main(args: Array[String]) {
+    val i = 1
+    val filename = s"${path}${i}"
+    val data = read(filename)
+    val labelindex = 4
+    val kList = Seq("health", "moderate", "moderate", "yes")
+    classify(kList, data,labelindex)
+  }
+
+  def classify(kList: Seq[String], data: List[List[String]], labelindex: Int): (String, Double) = {
+    val data1 = getData1(data)
+    val data2 = getData2(data, labelindex)
+    val columns = data.head.size
+    val data3 = data1(columns - 1)
     val data4 = data3.map {
       case (k, v) =>
         val p = data2(k).zip(kList).map { case (x, y) => x.getOrElse(y, 0.0) }
