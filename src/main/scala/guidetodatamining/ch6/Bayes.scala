86200a15fd7a49b8b1df59cefd9b88083e06d239
wuhao <hzwuhao8@gmail.com>
2016-01-14 14:24:49 +0800

    如何 运用到 实际的 项目中

--- src/main/scala/guidetodatamining/ch6/Bayes.scala
+++ src/main/scala/guidetodatamining/ch6/Bayes.scala
@@ -14,14 +14,14 @@ trait Bayes extends util.Log {
   val filename: String 
   val labelindex: Int 
   def getData1(data: List[List[String]]) = {
-    val columns = data.head.size
+   
     val data1 = for (i <- 0 to columns - 1) yield { data.groupBy(x => x(i)).map { case (k, v) => k -> v.size } }
 
     log.debug(s"data1=\n${data1}")
     data1
   }
   def getData2(data: List[List[String]], labelindex: Int) = {
-    val columns = data.head.size
+    
     val data2 = data.groupBy { x => x(labelindex) }.map {
       case (k, v) =>
         val total = v.size
@@ -49,14 +49,14 @@ trait Bayes extends util.Log {
   lazy val columns = data.head.size
   lazy val data3 = data1(labelindex)
 
-  def classify(kList: Seq[String]): (String, Double) = {
+  def classify(feature: Seq[String]): (String, Double) = {
 
     log.debug(s"data3=\n${data3}")
-
+    val sum = data3.map(_._2).sum
     val data4 = data3.map {
       case (k, v) =>
-        val p = data2(k).zip(kList).map { case (x, y) => x.getOrElse(y, 0.0) }
-        val r = p.product * (v.toDouble / data3.map(_._2).sum)
+        val p = data2(k).zip(feature).map { case (x, y) => x.getOrElse(y, 0.0) }
+        val r = p.product * (v.toDouble / sum )
         (k, r)
     }
     log.debug(s"data4=\n${data4}")
